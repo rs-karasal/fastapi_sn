e643a2f2194ef56d9da5dfd540138a168ad1e843
millennial <ereskarasal94@gmail.com>
2024-07-06 16:23:40 +0500

    refactor structure

--- code/migrations/env.py
+++ code/migrations/env.py
@@ -16,7 +16,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from models.models import metadata
+from models import metadata
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
 
